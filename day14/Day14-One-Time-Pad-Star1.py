915085ff04db81997cd9b647c6170da92ef0b8d2
bfinnema <bfinnema@cisco.com>
2016-12-15 20:21:57 +0100

    The solution for day 14.

---
+++ day14/Day14-One-Time-Pad-Star1.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[19]:
+
+import hashlib
+
+#print hashlib.md5("abc3231929").hexdigest()
+
+salt = 'ahsbgdzn'
+index = 0
+salt1 = 'abc'
+hash_table = []
+threes = []
+keys = []
+i = 0
+key_found = False
+
+while len(keys)<64:
+    hash_input = salt + str(index)
+    hash0 = hashlib.md5(hash_input).hexdigest()
+    #print i, hash_input, hash0
+    hash_table.append(hash0)
+    for three in threes:
+        three[2] += 1
+        x = 0
+        five_found = False
+        while x < (len(hash0)-4) and not five_found and three[2] < 1001 and three[3]:
+            if hash0[x] == hash0[x+1] == hash0[x+2] == hash0[x+3] == hash0[x+4]:
+                if hash0[x] == three[1]:
+                    keys.append([three[0],hash_table[three[0]]])
+                    five_found = True
+                    print "KEY found:", three[0], hash_table[three[0]], three[2]
+                    print i, hash_input, hash0
+                    three[3] = False
+            x +=1
+    x = 0
+    three_found = False
+    while x < (len(hash0)-2) and not three_found:
+        if hash0[x] == hash0[x+1] == hash0[x+2]:
+            threes.append([i,hash0[x],0,True])
+            three_found = True
+            #print "One with three found:", i, hash_input, hash0
+        x +=1
+    i += 1
+    index += 1
+    
+print "Numer of keys: %s" %len(keys)
+
+x = 0
+for key in keys:
+    print x, key[0], key[1]
+    x += 1
+#for three in threes:
+#    print three[0], hash_table[three[0]], three[2], three[3]
+
+
+# In[ ]:
+
+
+
